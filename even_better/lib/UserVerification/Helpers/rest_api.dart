c9ec4f4703576dc2af6f933ef1bd451f99ad5eb1
Slakstins <50281064+Slakstins@users.noreply.github.com>
2021-10-05 11:27:32 -0400

    started rest api integration

---
+++ even_better/lib/UserVerification/Helpers/rest_api.dart
@@ -0,0 +1,51 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+//documentation: https://flutter.dev/docs/cookbook/networking/send-data
+//if we need to work with responses, add stuff from here ^
+
+void registerEB(username, password, roseUsername) {
+  if (password == password) {
+    //TODO: add new credentials to firebase
+    print("registration time!");
+    _createAlbumRegisterEB(username, password, roseUsername);
+  } else {
+    //TODO: tell the user the password does not match
+
+  }
+}
+
+Future<http.Response> _createAlbumRegisterEB(username, password, roseUsername) {
+  return http.post(
+    Uri.parse(
+        'http://ec2-3-137-199-220.us-east-2.compute.amazonaws.com:5000/users/signup'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'username': username,
+      'rose-username': roseUsername,
+      'password': password,
+    }),
+  );
+}
+
+void loginEB(username, password) {
+  //verify account with our FireBase
+  _createAlbumLoginEB(username, password);
+  print("Even better username: ${username}");
+}
+
+Future<http.Response> _createAlbumLoginEB(username, password) {
+  return http.post(
+    Uri.parse(
+        'http://ec2-3-137-199-220.us-east-2.compute.amazonaws.com:5000/users/signup'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'username': username,
+      'password': password,
+    }),
+  );
+}
--- even_better/lib/UserVerification/sign_up.dart
+++ even_better/lib/UserVerification/sign_up.dart
@@ -1,30 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'Helpers/rest_api.dart';
 import '../main.dart';
 import 'Helpers/labeled_text_field.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({
+    required this.roseUsername,
     Key? key,
   }) : super(key: key);
+
+  final roseUsername;
 
   @override
   State<SignUp> createState() => _SignUpState();
 }
 
 class _SignUpState extends State<SignUp> {
-  void _registerEB() {
-    if (verifyPasswordController.text == passwordController.text) {
-      //TODO: add new credentials to firebase
-      var username = usernameController.text;
-      var password = passwordController.text;
-      print("registration time!");
-    } else {
-      //TODO: tell the user the password does not match
-
-    }
-  }
-
   @override
   void dispose() {
     usernameController.dispose();
@@ -72,7 +64,12 @@ class _SignUpState extends State<SignUp> {
             Container(
                 margin: const EdgeInsets.only(top: 40),
                 child: ElevatedButton(
-                    onPressed: _registerEB, child: const Text("Sign Up"))),
+                    onPressed: () {
+                      print(widget.roseUsername);
+                      registerEB(usernameController.text,
+                          passwordController.text, widget.roseUsername);
+                    },
+                    child: const Text("Sign Up"))),
           ],
         ),
       ),
