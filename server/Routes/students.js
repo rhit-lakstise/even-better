e98d6882f91bb12c996049c5538d0dfc345cc7d3
jamari-morrison <RedCater360@yahoo.com>
2021-11-11 09:49:20 -0500

    fix student route

--- server/Routes/students.js
+++ server/Routes/students.js
@@ -1,14 +1,30 @@
 const Student = require('../Models/Student');
+const User = require('../Models/User');
+
 const express = require('express');
 const router = express.Router();
+var nodemailer = require('nodemailer');
 
 
 
 router.get('/all', async (req, res) => {
+  try{
+      const students = await Student.find();
+      console.log("got mail")
+      res.json(students);
+  } catch(err){
+      res.json({message: "Error!"})
+  }
+})
+
+router.get('/list', async (req, res) => {
     try{
-        const students = await Student.find();
-        res.json(users);
+      console.log(req.query.year.toString())
+        const students = await Student.find({year: req.query.year});
+        console.log(students)
+        res.json(students);
     } catch(err){
+      console.log(err)
         res.json({message: "Error!"})
     }
 })
@@ -16,10 +32,12 @@ router.get('/all', async (req, res) => {
 router.post('/create', (req, res) => {
     console.log('creating student')
     console.log(req.body)
-    const user = new User({
-        "first-name": req.body['first-name'],
-        "last-name": req.body['last-name'],
-        "rose-username": req.body['rose-username']
+    const user = new Student({
+        "name": req.body['name'],
+        "rose-username": req.body['rose-username'],
+        "year": req.body['year'],
+        "major": req.body['major'],
+        "status": "Invite",
     });
 
     user.save()
@@ -31,4 +49,53 @@ router.post('/create', (req, res) => {
     })
 })
 
-module.exports = router;
+router.post('/checkExist', async (req, res) =>{
+  var exists = await Student.find({"rose-username": req.body['rose-username']});
+  var isTaken = await User.find({"rose-username": req.body['rose-username']});
+  if(exists.length > 0 && isTaken.length == 0) res.json({message: true})
+  else res.json({message: false})
+})
+
+//create post / get post, create tags / get tags, 
+router.post('/invite', async (req, res) => {
+
+    var toUpdate = await Student.find({"rose-username": req.body['rose-username']});
+    toUpdate[0]['status'] = "pending";
+    const result = await Student.updateOne({"_id": toUpdate[0]['_id']}, toUpdate[0])
+  .catch(err => {
+      res.json({message: err})
+  })
+    console.log(result)
+    
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'UtsukushiiNee@gmail.com',
+          pass: 'ilovekousei123'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'Kaori Miyazono',
+        to: req.body['rose-username']+'@rose-hulman.edu',
+        subject: req.body['inviter']+' Invites you to join Even Better',
+        text: 'That was easy!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+          res.json({message: error});
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      
+
+      res.json({
+          message: 'Successfully invited user!'
+      });
+    
+})
+
+module.exports = router; 
